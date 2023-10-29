995c01cfcddc2d85fc99bf98cffe7f79582dea97
RavelloH <hyh20060327@qq.com>
2023-10-29 13:28:52 +0800

    Feat. 重构

--- index.js
+++ index.js
@@ -1,38 +1,101 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-function chooseLargeFile(folders) {
-  const filesMap = new Map(); // 存储每个文件名和对应的大小
+// 获取命令行参数
+const folder = process.argv[2];
 
-  // 扫描每个文件夹
-  folders.forEach(folder => {
-    const files = fs.readdirSync(folder);
-    
+// 创建逐行读取的接口
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// 递归遍历文件夹
+function traverseFolder(folderPath) {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error('Error reading folder:', err);
+      return;
+    }
+
+    const fileMap = new Map();
+
+    // 遍历文件
     files.forEach(file => {
-      const filePath = path.join(folder, file);
-      const stats = fs.statSync(filePath);
-      
-      if (!filesMap.has(file) || stats.size > filesMap.get(file).size) {
-        filesMap.set(file, { path: filePath, size: stats.size });
-      }
-    });
-  });
+      const filePath = path.join(folderPath, file);
 
-  // 删除小文件并保存大文件
-  filesMap.forEach(file => {
-    const { path, size } = file;
-    
-    filesMap.forEach(otherFile => {
-      if (otherFile.size < size && otherFile.path !== path) {
-        fs.unlinkSync(otherFile.path);
-      }
+      // 判断是否为文件
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error('Error reading file:', filePath, err);
+          return;
+        }
+
+        if (stats.isFile()) {
+          const fileName = path.basename(file, path.extname(file));
+          const fileSize = stats.size;
+
+          // 存储文件信息
+          if (!fileMap.has(fileName)) {
+            fileMap.set(fileName, {
+              path: filePath,
+              size: fileSize
+            });
+          } else {
+            const existingFile = fileMap.get(fileName);
+
+            // 比较文件大小
+            if (fileSize < existingFile.size) {
+              // 删除较小的同名文件
+              deleteFile(filePath);
+            } else if (fileSize > existingFile.size) {
+              deleteFile(existingFile.path);
+              fileMap.set(fileName, {
+                path: filePath,
+                size: fileSize
+              });
+            }
+          }
+        }
+      });
     });
+
+    // 遍历完成后进行操作确认
+    if (fileMap.size > 0) {
+      rl.question('是否删除较小的同名文件？（Y/N）', answer => {
+        if (answer.toLowerCase() === 'y') {
+          deleteFiles(fileMap);
+        } else {
+          console.log('已取消删除操作');
+        }
+
+        rl.close();
+      });
+    } else {
+      console.log('没有找到同名文件');
+      rl.close();
+    }
   });
 }
 
-// 从命令行参数中获取文件夹路径
-const folders = process.argv.slice(2);
+// 删除文件
+function deleteFile(filePath) {
+  fs.unlink(filePath, err => {
+    if (err) {
+      console.error('Error deleting file:', filePath, err);
+    } else {
+      console.log('文件已成功删除:', filePath);
+    }
+  });
+}
 
-chooseLargeFile(folders);
+// 删除所有较小的同名文件
+function deleteFiles(fileMap) {
+  fileMap.forEach(file => {
+    deleteFile(file.path);
+  });
+}
 
-module.exports = chooseLargeFile;
+// 开始遍历文件夹
+traverseFolder(folder);
